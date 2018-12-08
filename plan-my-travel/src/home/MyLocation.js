3b6fb63844f1a8a88e5806de4b8f24c2c3979247
Swapnil Abnave <imswapab@gmail.com>
2018-12-08 12:28:52 +0100

    Update code logs, trying to fix test

---
+++ plan-my-travel/src/home/MyLocation.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+const MAP_API = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2';
+
+export default class MyLocation extends Component {
+    state = {
+        location: ''
+    };
+
+    componentDidMount() {
+        /* geolocation is available */
+        if ('geolocation' in navigator) {
+            const location = navigator.geolocation;
+            if (location) {
+                location.getCurrentPosition(
+                    position => {
+                        console.log('position.coords', position.coords);
+                        this.setState(
+                            {
+                                longitude: position.coords.longitude,
+                                latitude: position.coords.latitude
+                            },
+                            () => this.reverseGeocode()
+                        );
+                    },
+                    error => {
+                        this.setState({
+                            location: 'error'
+                        });
+                    }
+                );
+            }
+        } else {
+            this.setState({
+                location: 'geolocation error'
+            });
+            /* geolocation IS NOT available */
+        }
+    }
+
+    reverseGeocode = () => {
+        fetch(
+            `${MAP_API}&lat=${this.state.latitude}&lon=${this.state.longitude}`
+        )
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log('data', data.address);
+
+                this.setState({
+                    location: `${data.address.city}, ${data.address.country}`
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    location: 'geolocation error'
+                });
+            });
+    };
+
+    render() {
+        return (
+            <input
+                placeholder="Where are you right now?"
+                data-testid="my-current-location"
+                defaultValue={this.state.location}
+            />
+        );
+    }
+}
